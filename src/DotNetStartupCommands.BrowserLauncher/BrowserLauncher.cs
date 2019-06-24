bf8f297284cd9fd52d587d8951964498f85f97b8
Seth Steenken <sethsteenken@gmail.com>
2019-06-23 20:40:08 -0400

    Added browserlauncher project

---
+++ src/DotNetStartupCommands.BrowserLauncher/BrowserLauncher.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetStartupCommands.BrowserLauncher
+{
+    public sealed class BrowserLauncher
+    {
+        private readonly ILogger<BrowserLauncher> _logger;
+
+        public BrowserLauncher(ILogger<BrowserLauncher> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Launch(string[] args)
+        {
+
+        }
+    }
+}
